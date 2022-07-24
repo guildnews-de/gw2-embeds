4489b26e7fce8f83a0d543a3dd78bddd950bd5fe
thom-10 <thomaszehnder@outlook.de>
2022-07-24 20:45:05 +0200

    add mastery icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,11 @@ import gemsIcon from './gems.png';
 import karmaIcon from './karma.png';
 import laurelIcon from './laurel.png';
 import titleIcon from './title.png';
+import centralIcon from './mpCentral.png';
+import hotIcon from './mpHot.png';
+import pofIcon from './mpPof.png';
+import iceIcon from './mpIce.png';
+import eodIcon from './mpEod.png';
 
 export default {
   ap: {
@@ -29,5 +34,30 @@ export default {
     src: titleIcon,
     color: 'DarkGoldenRod',
     text: 'Titel',
+  },
+  central: {
+    src: centralIcon,
+    color: 'DarkGoldenRod',
+    text: 'Zentraltyria-Beherrschungspunkt',
+  },
+  hot: {
+    src: hotIcon,
+    color: 'DarkGoldenRod',
+    text: 'HoT-Beherrschungspunkt',
+  },
+  pof: {
+    src: pofIcon,
+    color: 'DarkGoldenRod',
+    text: 'PoF-Beherrschungspunkt',
+  },
+  ice: {
+    src: iceIcon,
+    color: 'DarkGoldenRod',
+    text: 'Eisbrut-Beherrschungspunkt',
+  },
+  eod: {
+    src: eodIcon,
+    color: 'DarkGoldenRod',
+    text: 'Eod-Beherrschungspunkt',
   },
 };
