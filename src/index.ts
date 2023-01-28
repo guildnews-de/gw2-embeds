1276ad3e86717977cdf6024cc90dae18aa8dabb3
thom-10 <thomaszehnder@outlook.de>
2023-01-28 12:28:18 +0100

    bump version 0.3

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
-// const findScriptBaseUrl = () => {
-//   if (document.getElementById('gw2-embeds-js')) {
-//     const script = document.getElementById(
-//       'gw2-embeds-js',
-//     ) as HTMLScriptElement;
-//     return `${script.src.substring(0, script.src.lastIndexOf('/'))}/`;
-//   }
-//   const scripts = document.getElementsByTagName('script');
-//   for (let i = 0; i < scripts.length; i++) {
-//     const script = scripts[i];
-//     if (script.src.includes('gw2-embeds.js')) {
-//       return script.src.replace('gw2-embeds.js', '');
-//     }
-//   }
-
-//   throw new Error('Could not find gw2-embeds script.');
-// };
-
 const init = () => {
-  // __webpack_public_path__ = findScriptBaseUrl();
-  // document.__publicPath = __webpack_public_path__;
   import('./bootstrap').then((module) => module.default());
 };
 
